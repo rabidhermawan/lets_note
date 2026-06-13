326743d0e5a078a80be87c65b1112aa0dfa56a1f
R. Abid Hermawan <97014247+rabidhermawan@users.noreply.github.com>
2026-06-14 03:08:42 +0800

    feat: experimenting on new reminder list (with delete options)
    Resemble an actual reminder now. Checkbox is non functional but notes can actually be deleted

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:lets_note/pages/note/note_view.dart' show NoteView;
 import 'package:lets_note/route.dart';
 import 'package:provider/provider.dart';
 
-import 'package:lets_note/pages/note/note_list.dart';
+// import 'package:lets_note/pages/note/note_list.dart';
+import 'package:lets_note/pages/reminder/reminder_list.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +30,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Let's Note!"), centerTitle: true),
-      body: NoteList(),
+      body: ReminderList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
           Navigator.push(
---
+++ lib/pages/reminder/reminder_list.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// Database
+import 'package:lets_note/models/app_db.dart';
+
+// Route
+import 'package:lets_note/pages/note/note_view.dart';
+
+class ReminderList extends StatefulWidget {
+  const ReminderList({super.key});
+
+  @override
+  State<ReminderList> createState() => _ReminderListState();
+}
+
+class _ReminderListState extends State<ReminderList> {
+  late final AppDatabase _db = context.read<AppDatabase>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+            child: StreamBuilder<List<NoteData>>(
+              stream: _db.watchAllNotes(),
+              builder: (context, snapshot) {
+                final List<NoteData>? notes = snapshot.data;
+                if (snapshot.connectionState != ConnectionState.active) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                } else if (notes != null) {
+                  return ListView.builder(
+                    itemCount: notes.length,
+                    itemBuilder: (context, index) {
+                      final note = notes[index];
+                      return _ReminderCard(rowObject: note);
+                    },
+                  );
+                } else {
+                  return Center(child: Text("No notes"));
+                }
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ReminderCard extends StatefulWidget {
+  final NoteData _rowObject;
+
+  const _ReminderCard({required this._rowObject});
+
+  @override
+  State<_ReminderCard> createState() => _ReminderCardState();
+}
+
+class _ReminderCardState extends State<_ReminderCard> {
+  bool _foo = false;
+
+  late final AppDatabase _db = context.read<AppDatabase>();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      margin: const EdgeInsets.only(top: 8.0),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.cyan[100],
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Row(
+        children: [
+          Checkbox(
+            checkColor: Colors.white,
+            value: _foo,
+            onChanged: (value) {
+              setState(() {
+                _foo = value!;
+              });
+            },
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NoteView(
+                      rowObject: widget._rowObject.toCompanion(true),
+                    ),
+                  ),
+                );
+              },
+              child: Text(
+                widget._rowObject.title.toString(),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  wordSpacing: -1,
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text("Delete Reminder"),
+                  content: const Text("Are you sure you want to delete it?"),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        _db.deleteNote(widget._rowObject.id.toInt());
+                        Navigator.pop(context);
+                      },
+                      child: const Text('OK'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text('Cancel'),
+                    ),
+                  ],
+                ),
+              );
+            },
+            icon: Icon(Icons.delete),
+          ),
+        ],
+      ),
+    );
+  }
+}
